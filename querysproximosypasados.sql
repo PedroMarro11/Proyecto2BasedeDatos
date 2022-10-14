15791483ae15445f2dd307bd34ecc6c4b3c9ad80
Raul <san21401@uvg.edu.gt>
2022-10-14 16:55:22 -0600

    Querys en sql
    Querys para ver sesiones proximas y pasadas

---
+++ querysproximosypasados.sql
@@ -0,0 +1,29 @@
+---------Proximas---------------------
+select ses.idsesion,ses.idinstructor,ses.fecha,ses.hora,ses.duracion
+from sesion as ses 
+inner join(
+    select* 
+    from 
+        usuariosesion as us
+    inner join sesion as s on us.idsesion=s.idsesion
+    inner join usuario as u on us.idusuario=u.idusuario
+    where 
+        s.fecha>current_date
+) as k
+on ses.fecha=k.fecha
+order by ses.fecha asc
+--------Pasadas----------
+select ses.idsesion,ses.idinstructor,ses.fecha,ses.hora,ses.duracion
+from sesion as ses 
+inner join(
+    select* 
+    from 
+        usuariosesion as us
+    inner join sesion as s on us.idsesion=s.idsesion
+    inner join usuario as u on us.idusuario=u.idusuario
+    where 
+        s.fecha<current_date
+) as k
+on ses.fecha=k.fecha
+order by ses.fecha desc
+
