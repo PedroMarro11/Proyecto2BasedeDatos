7250749b73378460b8db2551e32064ba56febce3
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-10-15 13:58:51 -0600

    Update querysproximosypasados.sql
    se cambiaron las queries para ser más efectivas

--- querysproximosypasados.sql
+++ querysproximosypasados.sql
@@ -12,6 +12,16 @@ inner join(
 ) as k
 on ses.fecha=k.fecha
 order by ses.fecha asc
+--mejorado
+SELECT s1.idsesion, s1.nombre, s1.apellido, s1.categoria, s1.fecha, EXTRACT(WEEK FROM s1.fecha) as semana, s1.hora, s1.duracion FROM usuariosesion as us
+    LEFT JOIN (
+        SELECT s.idsesion, ins.nombre, ins.apellido, cat.nombre as categoria, s.fecha, s.hora, s.duracion FROM sesion as s
+            LEFT JOIN instructor as ins ON s.idinstructor = ins.idinstructor
+            LEFT JOIN categoria as cat ON s.idcategoria = cat.idcategoria)s1
+    ON us.idsesion = s1.idsesion
+WHERE EXTRACT(WEEK FROM s1.fecha) = EXTRACT(WEEK FROM CURRENT_DATE)
+    AND us.idusuario = 1
+ORDER BY s1.fecha, s1.hora ASC
 --------Pasadas----------
 select ses.idsesion,ses.idinstructor,ses.fecha,ses.hora,ses.duracion
 from sesion as ses 
@@ -27,3 +37,13 @@ inner join(
 on ses.fecha=k.fecha
 order by ses.fecha desc
 
+--mejorado
+SELECT s1.idsesion, s1.nombre, s1.apellido, s1.categoria, s1.fecha, EXTRACT(WEEK FROM s1.fecha) as semana, s1.hora, s1.duracion FROM usuariosesion as us
+    LEFT JOIN (
+        SELECT s.idsesion, ins.nombre, ins.apellido, cat.nombre as categoria, s.fecha, s.hora, s.duracion FROM sesion as s
+            LEFT JOIN instructor as ins ON s.idinstructor = ins.idinstructor
+            LEFT JOIN categoria as cat ON s.idcategoria = cat.idcategoria)s1
+    ON us.idsesion = s1.idsesion
+WHERE s1.fecha < CURRENT_DATE
+    AND us.idusuario = 1
+ORDER BY s1.fecha, s1.hora DESC
