b31d2046c9356507c477601f4f268683c168b2b5
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-11-20 18:22:56 -0600

    Create CreacionInstructores.sql
    Script para crear los instructores que faltaban

---
+++ CreacionInstructores.sql
@@ -0,0 +1,12 @@
+INSERT INTO instructor(idinstructor, nombre, apellido, activo, fechainicio)
+VALUES (7, 'Dwayne', 'Johnson', true, '2022-10-15'),
+(8, 'Clark', 'Kent', true, '2022-10-15'),
+(9, 'Chris', 'Heria', true, '2022-10-15'),
+(10, 'Peter', 'Parker', true, '2022-10-15'),
+(11, 'Diana', 'Prince', true, '2022-10-15'),
+(12, 'Natasha', 'Romanoff', true, '2022-10-15'),
+(13, 'Kara', 'Kent', true, '2022-10-15'),
+(14, 'James', 'Howlett', true, '2022-10-15'),
+(15, 'Steve', 'Rogers', true, '2022-10-15')
+
+SELECT * FROM instructor ORDER BY idinstructor
